01275314827d9c80ad2a7534e088be050413d35f
NIK-AL-GIT <nik.al.git@gmail.com>
2024-12-26 03:45:22 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const menu = document.getElementById('menu');
     const headerMenu = document.getElementById('header_menu');
 
+    const buttons = document.querySelectorAll("button");
+
+    const twitchBut = document.querySelector(".twitch");
+    const robloxBut = document.querySelector(".roblox");
+    const asusBut = document.querySelector(".asus");
+    const canonBut = document.querySelector(".canon");
+    const microsoftBut = document.querySelector(".microsoft");
+
     if (menu && headerMenu) {
         menu.addEventListener('click', function(event) {
             event.stopPropagation();
@@ -12,6 +20,39 @@ document.addEventListener('DOMContentLoaded', function() {
             if (!headerMenu.contains(event.target) && event.target.id !== 'menu') {
                 headerMenu.classList.remove('show');
             }
+        });
+    }
+
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            window.location.href = "error.html";
+        });
+    });
+
+      
+    if (twitchBut) {
+        twitchBut.addEventListener("click", function() {
+            window.location.href = "https://www.twitch.tv/";
+        });
+    }
+    if (robloxBut) {
+        robloxBut.addEventListener("click", function() {
+            window.location.href = "https://www.roblox.com/";
+        });
+    }
+    if (asusBut) {
+        asusBut.addEventListener("click", function() {
+            window.location.href = "https://www.asus.com/";
+        });
+    }
+    if (canonBut) {
+        canonBut.addEventListener("click", function() {
+            window.location.href = "https://www.canon.com.by/";
+        });
+    }
+    if (microsoftBut) {
+        microsoftBut.addEventListener("click", function() {
+            window.location.href = "https://www.microsoft.com/ru-ru";
         });
     }
 });
